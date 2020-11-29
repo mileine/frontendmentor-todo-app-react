448b54d6cc0f500af6f1178a67e80f504466a7c0
mileine <43147648+mileine@users.noreply.github.com>
2020-11-29 01:27:14 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './styles/App.scss';
 
-function App() {
+const App = () => {
+  const [theme, setTheme] = useState('light')
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light'? 'dark': 'light')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`container ${theme}`}>
+      <div className="main">
+        <h1 className="title">TODO</h1>
+      </div>      
     </div>
   );
 }
 
-export default App;
+export default App
